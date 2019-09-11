f0d36f2d0463e7655b7b637ec54effe072f7777f
sheela senga <SSengamalam@spartaglobal.com>
2019-09-11 21:33:46 +0100

    update

--- ScrabbleGame/Welcome.xaml.cs
+++ ScrabbleGame/Welcome.xaml.cs
@@ -20,13 +20,16 @@ namespace ScrabbleGame
     public partial class Welcome : Window
     {
         string[] words;
+        MainWindow sw = new MainWindow();
         public string currentword;
         private int wrongGuess = 0;
-        private string copyCurrent = "";
-        private int guessCount = 0;
+       // private string copyCurrent = "";
+        
+       // private int guessCount = 0;
         List<char> finalwords;
         List<int> index;
         int count = 0;
+        int hasWord = 0;
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
 
@@ -52,17 +55,17 @@ namespace ScrabbleGame
             string newword = words[rnd.Next(words.Length)];
             char[] wordChars = newword.ToCharArray();
             int len = wordChars.Length;
-            string random = new string(words.ToString().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray());
+            // string random = new string(words.ToString().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray());
             int rnumber = rnd.Next(0, len);
-            // display the generated word in textbox
+            // display the generated scrabbled word in textbox
             while (true)
             {
-                if(!(index.Contains(rnumber)))
+                if (!(index.Contains(rnumber)))
                 {
                     index.Add(rnumber);
                     finalwords.Add(wordChars[rnumber]);
                     count++;
-                    if(count > 6)
+                    if (count > 6)
                     {
                         break;
                     }
@@ -70,95 +73,103 @@ namespace ScrabbleGame
                 rnumber = rnd.Next(0, len);
             }
             sb1 = new StringBuilder();
-            foreach(char ch in finalwords)
+            foreach (char ch in finalwords)
             {
                 sb1.Append(ch.ToString());
             }
-                
+
             wordtxt.Text = sb1.ToString();
 
             string real_words = words[rnd.Next(words.Length)];
 
-            //string rand_num = rnd.Next(0,words.Length);
-            //wordtxt.Text = random;
-            // MessageBox.Show(random);
             lengthTxt.Text = len.ToString();
-            //guessleftTxt.Text = guessCount.ToString();
-            //guessleftTxt.Text = " ";
-            ////hide the generated word using _
+
+
+            //hide the generated word using _
             for (int i = 0; i < wordChars.Length; i++)
             {
                 WordLbl.Text += ("_ ");
             }
             wordbtn.IsEnabled = false;
 
-          
         }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
         }
 
-        private void Wordchoice()
-        {
-            wrongGuess = 0;
-            int guessIndex = (new Random()).Next(words.Length);
-            currentword = words[guessIndex];
-            copyCurrent = "";
-            for (int index = 0; index < currentword.Length; index++)
-            {
-                copyCurrent += "_";
-                guessCount--;
-            }
-            displayword();
-        }
-        
-
-        private void displayword()
-        {
-
-            for (int index = 0; index < currentword.Length; index++)
-            {
-                WordLbl.Text += copyCurrent.Substring(index, 1);
-                WordLbl.Text += " ";
-
-            }
-        }
+    
         private void GuessClick(object sender, EventArgs e)
         {
             Button b = (Button)sender;
-           char charClicked = b.Content.ToString()[0];
-            b.IsEnabled = false;
+            char charClicked = b.Content.ToString().ElementAt(0);
+             b.IsEnabled = false;
+            //  MessageBox.Show(charClicked.ToString());
+         
+            
+            // button clicks 
+            if (finalwords.Contains(charClicked))
+            {
+                MessageBox.Show("Correct Letter");
+
+                // Add it to the _ _ _ _ 
+            }
+
+            char[] correctCharArray = new char[finalwords.Count];
+            for (int k = 0; k < correctCharArray.Length; k++)
+            {
+                correctCharArray[k] = '_';
+
+            }
+
+            for (int i = 0; i < finalwords.Count; i++)
+            {
+                if (finalwords[i] == charClicked)
+                {
+
+                    MessageBox.Show("Correct letter at index " + i);
+                    correctCharArray[i] = charClicked;
+                }
+            }
+
+            // string charsStr = new string(correctCharArray);
+            WordLbl.Text = new String(correctCharArray);
+        
+    
+        {
+         wrongGuess++;
+
+        }
            
-
-            //if (currentword.Contains(choice.Text))
-            //   {
-            //  char[] temp = copyCurrent.ToCharArray();
-            //  char[] find = currentword.ToCharArray();
-            // char guessChar = wordbtn.Content.ElementAt(0);
-            // for (int index = 0; index < find.Length; index++)
-            //  {
-            //   if (find[index] == guessChar)
-            //    {
-            //      temp[index] = guessChar;
-            //  }
-            //   }
-
-            wrongGuess++;
-            if (wrongGuess > 7)
+            if (wrongGuess == 8)
             {
                 MessageBox.Show("wrong guess, You lost");
+        
+                sw.Show();
+                this.Close();
+              
             }
             else
             {
-                //    // MessageBox.Show("You won!");
+             if (words.Equals(finalwords))
+              
+              
+                {
+                    MessageBox.Show("You Win!");
+                }
+
             }
-
-
         }
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+      
+            sw.Show();
+            this.Close();
         }
     }
 }
